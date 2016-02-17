b3404dfb3e914249d02e8ceda7e9cd2ff1760126
Jasper Moelker <j.b.moelker@activeids.nl>
2016-02-17 12:05:46 +0100

    docs(examples): bootstrap v3 example linking to documentation sections on getbootstrap.com

---
+++ examples/bootstrap-v3/debug.js
@@ -0,0 +1,28 @@
+(function(viewLinker){
+    'use strict';
+
+    var mapping = {
+        '.navbar': {
+            name: 'Navbar',
+            url: 'components/#navbar'
+        },
+        '[data-toggle="collapse"]': {
+            name: 'Collapse',
+            url: 'javascript/#collapse'
+        },
+        '.carousel': {
+            name: 'Carousel',
+            url: 'javascript/#carousel'
+        },
+        '.media': {
+            name: 'Media object',
+            url: 'components/#media'
+        }
+    };
+    var options = {
+        'baseUrl': 'https://getbootstrap.com/'
+    };
+
+    viewLinker(mapping, options);
+
+}(window.viewLinker));
